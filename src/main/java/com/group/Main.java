e8768580f12e378b979e08824748925dc3f1e47d
Alex Padilla <alex.padilla@epn.edu.ec>
2023-11-28 12:30:01 -0500

    main: calculator and arithmetic menu implementation

---
+++ src/main/java/com/group/Main.java
@@ -0,0 +1,11 @@
+package com.group;
+
+import com.group.utils.ArithmeticMenu;
+
+public class Main {
+    public static void main(String[] args) {
+        ArithmeticMenu arithmeticMenu = new ArithmeticMenu();
+
+        arithmeticMenu.run();
+    }
+}
---
+++ src/main/java/com/group/utils/Calculator.java
@@ -0,0 +1,50 @@
+package com.group.utils;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void performAddition() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        double result = num1 + num2;
+        System.out.println("Result: " + result);
+    }
+
+    public static void performSubtraction() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        double result = num1 - num2;
+        System.out.println("Result: " + result);
+    }
+
+    public static void performMultiplication() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        double result = num1 * num2;
+        System.out.println("Result: " + result);
+    }
+
+    public static void performDivision() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the dividend: ");
+        double dividend = scanner.nextDouble();
+        System.out.print("Enter the divisor: ");
+        double divisor = scanner.nextDouble();
+
+        if (divisor != 0) {
+            double result = dividend / divisor;
+            System.out.println("Result: " + result);
+        } else {
+            System.out.println("Error: Cannot divide by zero.");
+        }
+    }
+}
